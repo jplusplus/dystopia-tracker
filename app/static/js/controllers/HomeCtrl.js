1daa01a81e01275f18b3c597ea8386e116cf9e97
Paul Joannon <paul.joannon@gmail.com>
2014-04-01 18:16:49 +0200

    clean HomeCtrl

--- app/static/js/controllers/HomeCtrl.js
+++ app/static/js/controllers/HomeCtrl.js
@@ -3,6 +3,13 @@ angular.module('dystopia-tracker').controller('HomeCtrl', ['$scope', 'Prediction
     $scope.predictions = [];
     $scope.sources = [];
 
+    var titles = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        local: []
+    });
+    titles.initialize();
+
     Prediction.get({}, function(data) {
         $scope.predictions = data.results;
     });
@@ -10,43 +17,20 @@ angular.module('dystopia-tracker').controller('HomeCtrl', ['$scope', 'Prediction
     Categories.get({}, function(data) {
         $scope.categories = data.results;
     });
-    
+
     Sources.get({}, function(data) {
         $scope.sources = data.results;
         titles.add(data.results);
     });
-    
-    // constructs the suggestion engine
-    var titles = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    // `states` is an array of state names defined in "The Basics"
-    local: []
-});
- 
-    // kicks off the loading/processing of `local` and `prefetch`
-    titles.initialize();
-    
+
     // Typeahead options object
-    $scope.exampleOptions = {
-    highlight: true
+    $scope.typeahedOptions = {
+        highlight: true
     };
 
-    // Single dataset example
-    $scope.exampleData = {
-    displayKey: 'title',
-    source: titles.ttAdapter()
-  };
- 
-    $('#bloodhound .typeahead').typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-     },
-    {
-        name: 'titles',
+    // Typeahead data object
+    $scope.typeaheadData = {
         displayKey: 'title',
         source: titles.ttAdapter()
-    });
-    
+    };
 }]);
