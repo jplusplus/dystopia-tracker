fdebdc64ec85a0cd0b01bf62c0396335aee87e53
David Bauer <david.bauer@tageswoche.ch>
2014-04-01 17:45:40 +0200

    bootstrap added

--- app/static/js/controllers/HomeCtrl.js
+++ app/static/js/controllers/HomeCtrl.js
@@ -13,5 +13,40 @@ angular.module('dystopia-tracker').controller('HomeCtrl', ['$scope', 'Prediction
     
     Sources.get({}, function(data) {
         $scope.sources = data.results;
+        titles.add(data.results);
     });
+    
+    // constructs the suggestion engine
+    var titles = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    // `states` is an array of state names defined in "The Basics"
+    local: []
+});
+ 
+    // kicks off the loading/processing of `local` and `prefetch`
+    titles.initialize();
+    
+    // Typeahead options object
+    $scope.exampleOptions = {
+    highlight: true
+    };
+
+    // Single dataset example
+    $scope.exampleData = {
+    displayKey: 'title',
+    source: titles.ttAdapter()
+  };
+ 
+    $('#bloodhound .typeahead').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+     },
+    {
+        name: 'titles',
+        displayKey: 'title',
+        source: titles.ttAdapter()
+    });
+    
 }]);
