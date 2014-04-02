5b64da400256c38bd6e658c938c3d5a3b4e7d2e7
David Bauer <david.bauer@tageswoche.ch>
2014-04-02 16:36:15 +0200

    begin pagination

--- app/static/js/controllers/HomeCtrl.js
+++ app/static/js/controllers/HomeCtrl.js
@@ -4,6 +4,8 @@ angular.module('dystopia-tracker').controller('HomeCtrl', ['$scope', 'Prediction
     $scope.editorspicks = [];
     $scope.sources = [];
     $scope.filters = {category:'', source__type:'', title:''};
+    $scope.filters.page = 0;
+    $scope.hideMoreButton = false;
 
     // TODO use multiple datasets so different source types appear grouped in typeahead: http://twitter.github.io/typeahead.js/examples/#multiple-datasets
     var titles = new Bloodhound({
@@ -22,22 +24,37 @@ angular.module('dystopia-tracker').controller('HomeCtrl', ['$scope', 'Prediction
         titles.add(data.results);
     });
 
-    $scope.update = function() {
+    $scope.update = function(reset) {
+        
+        // increment to the next page of the API
+        $scope.filters.page++; 
+        
         if (typeof($scope.filters.title) === 'object') {
             $scope.filters.title = $scope.filters.title.title;
         }
     	editorspick_filters = angular.copy($scope.filters);
     	editorspick_filters.editors_pick = 'True'; 
-
-		// get all predictions with selected filter applied
-	    Prediction.get($scope.filters).success(function(data) {
-            $scope.predictions = data.results;
-        });
-
-        // get all editor's pick with selected filter applied 
-        Prediction.get(editorspick_filters).success(function(data) {
-            $scope.editorspicks = data.results;
-        });
+		
+		if(reset==true) {
+			$scope.filters.page = 1;
+			$scope.predictions = [];
+            $scope.editorspicks = [];
+        }
+        
+        // get all predictions with selected filter applied
+		Prediction.get($scope.filters).success(function(data) {
+		    $scope.predictions = $scope.predictions.concat(data.results);
+		});
+		
+		// get all editor's pick with selected filter applied 
+		Prediction.get(editorspick_filters).success(function(data) {
+		    $scope.editorspicks = $scope.editorspicks.concat(data.results);
+		});
+		
+		if (data.next==null) {
+		    $scope.hideMoreButton = true;
+		}
+         
     };
 
     $scope.update();
