0a77a7310e2763321b7bdee42f7c4d107047835a
David Bauer <david.bauer@tageswoche.ch>
2014-04-04 11:07:43 +0200

    display related content to single prediction

--- app/static/js/controllers/DetailsCtrl.js
+++ app/static/js/controllers/DetailsCtrl.js
@@ -5,24 +5,42 @@ angular.module('dystopia-tracker').controller('DetailsCtrl', ['$scope', 'Predict
     $scope.realisations = [];
     $scope.category = [];
     $scope.shareurls = [];
+    $scope.more = [];
+    $scope.filters = {exclude : $routeParams.id, title : '', author : '', category : ''};
     
     Prediction.get({id:$routeParams.id}).success(function(data) {
 		$scope.prediction = data;
-		console.log(data);
 		$scope.realisations = $scope.prediction.realisations;
 		getCategoryTitle($scope.prediction.category);
+		getMore("title",$scope.prediction.source.title);
+        getMore("author",$scope.prediction.source.author);
+        getMore("category",$scope.prediction.category);
     });	
     
     function getCategoryTitle(id) {
         Categories.get({id:id}, function(data) {
         $scope.category = data;
-        console.log($scope.category);
         });
     };
    
     // TODO function to find correct vertical position of various realisations
     
     // TODO load "more" cards (same title, same author, same category)
+    
+    
+    
+    function getMore(param,value) {
+        filters = {exclude : $routeParams.id} 
+        filters[param] = value;
+        console.log(filters);
+	    Prediction.get(filters).success(function(data) {
+		    $scope.more[param] = data.results;
+		    console.log(data.results);
+	    });    
+    }
+    
+    
+    
     
     // create specific sharer urls for social media
     $scope.shareurls.fb = "http://www.facebook.com/sharer.php?u=" + $location.absUrl();
