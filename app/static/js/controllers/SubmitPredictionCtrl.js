23c33a7984c5ef19ffcd0622c9cc680d424711ea
David Bauer <david.bauer@tageswoche.ch>
2014-04-04 16:23:44 +0200

    submit form template

---
+++ app/static/js/controllers/SubmitPredictionCtrl.js
@@ -0,0 +1,6 @@
+angular.module('dystopia-tracker').controller('SubmitPredictionCtrl', ['$scope', 'Prediction', 'Categories', 'Sources', '$rootScope', '$location', '$routeParams',
+                                                           function($scope, Prediction, Categories, Sources, $rootScope, $location, $routeParams) {
+    
+    $scope.language = $rootScope._lang;
+    
+}]); // it's the end of the code as we know it
