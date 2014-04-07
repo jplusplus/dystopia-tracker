4ed0e21b23ce77deac14bb33c80e0a289e2848ea
Paul Joannon <paul.joannon@gmail.com>
2014-04-07 10:31:52 +0200

    remove get function in Realisation service

--- app/static/js/services/Realisation.js
+++ app/static/js/services/Realisation.js
@@ -2,23 +2,6 @@ var a = angular.module('dystopia-tracker.services').service('Realisation', ['$ht
     var service = function() { };
     service.baseUrl = '/api/realisations/';
 
-    service.get = function(params) {
-        params = params || {};
-        var url = this.baseUrl;
-        var paramString = '';
-        if (params.id != null) {
-            url += String(params.id) + '/'
-            delete params.id;
-        }
-        for (var key in params) if (params.hasOwnProperty(key)) {
-            if (params[key] != null && params[key] != '') {
-                var sep = (paramString === '') ? '?' : '&';
-                paramString += sep + key + '=' + params[key];
-            }
-        }
-        return $http.get(url + paramString);
-    }
-
     service.post = function(data) {
         return $http.post(this.baseUrl, data);
     }
