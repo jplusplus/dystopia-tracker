be3fcaf42294e4980b064422c76e8f78588f06ff
David Bauer <david.bauer@tageswoche.ch>
2014-04-08 14:17:56 +0200

    work started on timeline

--- app/static/js/controllers/DetailsCtrl.js
+++ app/static/js/controllers/DetailsCtrl.js
@@ -15,11 +15,15 @@ angular.module('dystopia-tracker').controller('DetailsCtrl', ['$scope', 'Predict
     $scope.more = [];
     $scope.filters = {exclude : $routeParams.id, title : '', author : '', category : ''};
     $scope.language = $rootScope._lang;
+    /* create array to story year_published of source, year_predicted of prediction, and year_introduced of all realisations
+    including the corresponding descriptions */
+    $scope.alldates = [];
     
     Prediction.get({id:$routeParams.id}).success(function(data) {
 		$scope.prediction = data;
 		$scope.realisations = $scope.prediction.realisations;
-		getCategoryTitle($scope.prediction.category);
+		createYearsArray($scope.prediction,$scope.realisations);
+	    getCategoryTitle($scope.prediction.category);
 		getMore("title",$scope.prediction.source.title);
         getMore("author",$scope.prediction.source.author);
         getMore("category",$scope.prediction.category);
@@ -31,8 +35,17 @@ angular.module('dystopia-tracker').controller('DetailsCtrl', ['$scope', 'Predict
         });
     };
    
-    // TODO function to find correct vertical position of various realisations (wait for design)
+    function createYearsArray(prediction,realisations) {
+        // push all year values into the array
+        $scope.alldates.push({"year":prediction.source.year_published, "text": "", "img":prediction.source.image, "credit": ""});
+        $scope.alldates.push({"year":prediction.year_predicted, "text": "", "img":"", "credit": ""});
+        
+        for (i=0;i<realisations.length;i++) {
+    	    $scope.alldates.push({"year":realisations[i].year_introduced, "text": realisations[i].description_E, "img":realisations[i].image, "credit": realisations[i].username});    
+        };
+    };
     
+      
     function getMore(param,value) {
         filters = {exclude : $routeParams.id} 
         filters[param] = value;
