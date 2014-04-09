f2f80a93abc32e779f1a27211f97590759f98ecb
David Bauer <david.bauer@tageswoche.ch>
2014-04-09 14:40:36 +0200

    update columns and filters in the backend

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,20 +8,20 @@ admin.site.register(Category)
 class SourceAdmin(ImportExportModelAdmin):
     resource_class = SourceResource
     search_fields = ('title', 'author', 'year_published', 'description_E', 'description_D')
-    list_display = ('type', 'title', 'author', 'year_published')
+    list_display = ('title', 'author', 'year_published', 'type', 'image')
     list_filter = ('featured', 'type')
 admin.site.register(Source, SourceAdmin)
 
 class PredictionAdmin(ImportExportModelAdmin):
     resource_class = PredictionResource
-    search_fields = ('source__title', 'source__author', 'source__year_published', 'year_predicted', 'username', 'category__title_E', 'category__title_D')
-    list_display = ('id', 'headline_E', 'year_predicted', 'source', 'category', 'creation_date', 'username' )
-    list_filter = ('published', 'editors_pick', 'category')
+    search_fields = ('source__title', 'source__author', 'source__year_published', 'year_predicted', 'username', 'category__title_E', 'category__title_D', 'description_E', 'description_D')
+    list_display = ('id', 'headline_E', 'description_E', 'year_predicted', 'source', 'category', 'username', 'creation_date' )
+    list_filter = ('published', 'editors_pick', 'category', 'source')
 admin.site.register(Prediction, PredictionAdmin)
 
 class RealisationAdmin(ImportExportModelAdmin):
     resource_class = RealisationResource
     search_fields = ('description_E', 'description_D', 'year_introduced', 'prediction__year_predicted', 'prediction__source__title', 'prediction__source__author', 'username')
-    list_display = ('id', 'prediction', 'creation_date', 'username')
+    list_display = ('id', 'description_E', 'prediction', 'username', 'creation_date')
     list_filter = ('published',)
 admin.site.register(Realisation, RealisationAdmin)
