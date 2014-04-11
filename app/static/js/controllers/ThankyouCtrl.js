d09900f2129b150f5f9be5811cbc9dbd501bf79f
David Bauer <david.bauer@tageswoche.ch>
2014-04-11 10:58:22 +0200

    styling of sharing buttons

--- app/static/js/controllers/ThankyouCtrl.js
+++ app/static/js/controllers/ThankyouCtrl.js
@@ -5,6 +5,7 @@ angular.module('dystopia-tracker').controller('ThankyouCtrl', ['$scope', 'Predic
     $scope.prediction.id = parseInt($location.search().p), 
     $scope.shareurls = [],
     $scope.language = $rootScope._lang;
+    
 
     Prediction.get({id:$scope.prediction.id}).success(function(data) {
 		$scope.prediction = data;
@@ -14,10 +15,22 @@ angular.module('dystopia-tracker').controller('ThankyouCtrl', ['$scope', 'Predic
     function createSharingUrls(prediction) {
         var slugifyfilter = $filter('slugify');
         // TODO: add text and images to optimise sharing: http://ar.zu.my/how-to-really-customize-the-deprecated-facebook-sharer-dot-php/
-        var url = "http://www.dystopiatracker.com/" + $scope.language + "/p/" + $filter('slugify')($filter('reverse')(prediction.source.author)) + "/" + $filter('slugify')(prediction.source.title) + "/" + prediction.id;
-        $scope.shareurls.fb = "http://www.facebook.com/sharer.php?u=" + url;
-        $scope.shareurls.twi = "http://twitter.com/share?url=" + url;
-        $scope.shareurls.mail = "mailto:?Subject=Dystopia Tracker&Body=" + url;
+        $scope.url = "http://www.dystopiatracker.com/" + $scope.language + "/p/" + $filter('slugify')($filter('reverse')(prediction.source.author)) + "/" + $filter('slugify')(prediction.source.title) + "/" + prediction.id;
+       
+       if ($rootScope._lang == "D") {
+	    $scope.shareurls.description = "Deutsche Beschreibung"
+       }
+       else {$scope.shareurls.description = "English description"};
+       $scope.shareurls.picture = "";
+    
+       $scope.shareurls.fb = "https://www.facebook.com/dialog/feed?app_id=624040751022885&redirect_uri=" + $scope.url + "&display=page&link=" + $scope.url + "&name=Dystopia%20Tracker&description=" + $scope.shareurls.description + "&picture=" + $scope.shareurls.picture;
+       $scope.shareurls.twi = "https://twitter.com/intent/tweet?text=" + $scope.shareurls.description + "&url=" + $scope.url + "&via=dystopiatracker";
+       $scope.shareurls.mail = "mailto:?Subject=Dystopia Tracker&Body=" + $scope.shareurls.description + " --> " + $scope.url;
+       
+       
+       
+       
+       
     }
     
 }]); // it's the end of the code as we know it
