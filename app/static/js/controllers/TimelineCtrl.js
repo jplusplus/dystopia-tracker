96c4d51d54d8ae17c6a3e079e04aeb935be34090
David Bauer <david.bauer@tageswoche.ch>
2014-04-14 09:15:45 +0200

    create timeline partial

---
+++ app/static/js/controllers/TimelineCtrl.js
@@ -0,0 +1,6 @@
+angular.module('dystopia-tracker').controller('TimelineCtrl', ['$scope', 'Prediction', 'Categories', 'Sources', 'Realisation', '$rootScope', '$location', '$routeParams',
+                                                           function($scope, Prediction, Categories, Sources, Realisation, $rootScope, $location, $routeParams) {
+    
+        
+    
+}]); // it's the end of the code as we know it
