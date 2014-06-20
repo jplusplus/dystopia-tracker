6f8faaf84579c15aa0c1277b928892e466f01d55
Paul Joannon <paul.joannon@gmail.com>
2014-06-20 14:03:26 +0200

    node size depends on number of lines

--- app/static/js/directives/timeline.js
+++ app/static/js/directives/timeline.js
@@ -19,12 +19,16 @@ angular.module('dystopia-tracker').directive('timeline', ['$window', '$timeout',
                 bottom : 10
             };
 
-            this.d3_base_y = this.d3_svg_padding.top;
             this.d3_node_size = 3;
+            this.d3_base_y = this.d3_svg_padding.top + (this.d3_node_size / 2);
             this.d3_line_height = this.d3_node_size * 1.5;
 
             this.init = function(predictions) {
                 this._i = 0;
+
+                this.d3_node_size = _.max([3, 50 / (predictions.length / 5)]);
+                this.d3_base_y = this.d3_svg_padding.top + (this.d3_node_size / 2);
+                this.d3_line_height = this.d3_node_size * 1.5;
 
                 var height = this.d3_base_y + this.d3_svg_padding.bottom + this.d3_line_height * predictions.length + 75;
 
